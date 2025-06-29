82c1cdd0522da2e6fde513164f468e109f1ebe22
Amit Jape <japeamit@gmail.com>
2025-06-29 23:38:31 +0530

    Node Beginning MERN 8

--- Day11/welcome.js
+++ Day11/welcome.js
@@ -1,6 +1,17 @@
 console.log('hello');
+const generateRandomNum = require('./randomNum')
 
-// console.log(window); // frontend
-console.log(global) // backend
+console.log(generateRandomNum());
+console.log(`First Random : ${generateRandomNum()}`)
+console.log(`Second Random : ${generateRandomNum()}`)
+// ------------------------------------------------------------------
 
-console.log(process);
+const Math  = require('./calculation');
+
+console.log(Math.addit(6,3));
+console.log(Math.subbit(6,5));
+console.log(Math.multiply(6,5));
+console.log(Math.divide(6,5));
+
+// console.log(sub(6,2));
+
